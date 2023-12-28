6877050d14b68cb5a1af5953df1c2b492b19c682
Peter Brown <brow1067@gmail.com>
2023-12-28 10:36:08 -0700

    Fix typo and editor save

--- deps/ImGuiColorTextEdit/TextEditor.cpp
+++ deps/ImGuiColorTextEdit/TextEditor.cpp
@@ -104,7 +104,8 @@ std::string TextEditor::GetText(const Coordinates & aStart, const Coordinates & 
 		{
 			istart = 0;
 			++lstart;
-			result += '\n';
+            if(lstart < (int)mLines.size())
+			    result += '\n';
 		}
 	}
 
