93aef1351001e9eebe26b4572aeb4136cafeb868
Peter Brown <brow1067@gmail.com>
2024-01-05 11:10:19 -0700

    improve compiler output further, death animation / state

--- src/abcc.cpp
+++ src/abcc.cpp
@@ -54,10 +54,10 @@ int main(int argc, char** argv)
 #endif
 
 #ifndef NDEBUG
-    //psrc = "C:/Users/Brown/Documents/GitHub/abc/examples/test/main.abc";
+    psrc = "C:/Users/Brown/Documents/GitHub/abc/examples/test/main.abc";
     //psrc = "C:/Users/Brown/Documents/GitHub/abc/examples/platformer/main.abc";
     //psrc = "C:/Users/Brown/Documents/GitHub/abc/benchmarks/bubble3/bubble3.abc";
-    psrc = "C:/Users/Brown/Documents/GitHub/abc/tests/tests/struct_ref_bug.abc";
+    //psrc = "C:/Users/Brown/Documents/GitHub/abc/tests/tests/struct_ref_bug.abc";
     //pbin = "C:/Users/Brown/Documents/GitHub/abc/examples/test/blah.bin";
 #endif
 
@@ -137,9 +137,12 @@ int main(int argc, char** argv)
 
     printf("Compilation succeeded!\n");
     printf("======================\n");
-    printf("Data:    %7d bytes\n", (int)a.data_size());
-    printf("Code:    %7d bytes\n", (int)a.code_size());
-    printf("Debug:   %7d bytes\n", (int)a.debug_size());
+    printf("Dev Bin: %7d bytes  %6.1f KB\n", (int)a.data().size(), (double)a.data().size() / 1024);
+    printf("======================\n");
+    printf("Data:    %7d bytes  %6.1f KB\n", (int)a.data_size(), (double)a.data_size() / 1024);
+    printf("Code:    %7d bytes  %6.1f KB\n", (int)a.code_size(), (double)a.code_size() / 1024);
+    printf("Debug:   %7d bytes  %6.1f KB\n", (int)a.debug_size(), (double)a.debug_size() / 1024);
+    printf("======================\n");
     printf("Globals: %7d bytes\n", (int)a.globals_size());
     printf("Save:    %7d bytes\n", (int)a.save_size());
     printf("======================\n");
