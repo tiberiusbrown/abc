ef0c34439a9c6c2d95a404ab91d222deea634c07
Peter Brown <brow1067@gmail.com>
2024-04-19 13:47:42 -0600

    fix build script

--- interp_arduboy/_build.bat
+++ interp_arduboy/_build.bat
@@ -5,7 +5,7 @@ cd /d %~dp0
 arduino-cli.exe compile ^
     -b arduboy-homemade:avr:arduboy-fx . ^
     --output-dir "build" ^
-    --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x800639" ^
+    --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x80063d" ^
     --build-property compiler.c.extra_flags="-mtiny-stack" ^
     --build-property compiler.cpp.extra_flags="{compiler.c.extra_flags}" ^
     --build-property compiler.elf2hex.extra_flags="-R .beforedata"
@@ -13,7 +13,7 @@ arduino-cli.exe compile ^
 arduino-cli.exe compile ^
     -b arduboy-homemade:avr:arduboy-mini . ^
     --output-dir "build_mini" ^
-    --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x800639" ^
+    --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x80063d" ^
     --build-property compiler.c.extra_flags="-mtiny-stack" ^
     --build-property compiler.cpp.extra_flags="{compiler.c.extra_flags}" ^
     --build-property compiler.elf2hex.extra_flags="-R .beforedata"
