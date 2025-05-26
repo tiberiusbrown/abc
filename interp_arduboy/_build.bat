b3a4fbd1c4e3081514c00562474682a6efa59c64
Peter Brown <brow1067@gmail.com>
2025-05-25 21:27:38 -0600

    add build_s2_homemade.leonardo.sh1106.rx config

--- interp_arduboy/_build.bat
+++ interp_arduboy/_build.bat
@@ -35,6 +35,14 @@ arduino-cli.exe compile ^
     --build-property compiler.elf2hex.extra_flags="-R .beforedata"
 
 arduino-cli.exe compile ^
+    -b arduboy-homemade:avr:arduboy-homemade:boot=cathy3k,display=sh1106,flashselect=rx . ^
+    --output-dir "build_s2_homemade.leonardo.sh1106.rx" ^
+    --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x80063e" ^
+    --build-property compiler.c.extra_flags="-mtiny-stack" ^
+    --build-property compiler.cpp.extra_flags="{compiler.c.extra_flags}" ^
+    --build-property compiler.elf2hex.extra_flags="-R .beforedata"
+
+arduino-cli.exe compile ^
     -b arduboy-homemade:avr:arduboy-homemade:boot=cathy3k,display=sh1106,based_on=promicro_alt,flashselect=rx . ^
     --output-dir "build_s2_homemade.promicro_alt.sh1106.rx" ^
     --build-property compiler.c.elf.extra_flags="-Wl,--relax -Wl,--section-start=.beforedata=0x800100 -Wl,--section-start=.data=0x80063e" ^
